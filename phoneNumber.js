9eb999cc6822c10c30ba2b7685b16e408f7d64a8
OP-Oscar <oscarpena07@outlook.com>
2023-05-02 10:12:57 -0500

    solution commented

--- phoneNumber.js
+++ phoneNumber.js
@@ -17,36 +17,41 @@
 
 let numArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0] // => returns "(123) 456-7890"
 
-console.log(-1< 0 || -1 >10)
-
 //function that accepts an array of 10 digits
 function createPhoneNumber(arr){
+        //condition set to true that everything is ok
         let condition = 1
+        //set condition to false if numbers aren't ok
         for(val of arr){
             if(val < 0 || val > 10){
                 condition = 0
+                }
              }
-        }
-        console.log(condition)
+
+        //if to make sure lenght ok
         if(arr.length != 10){
             return "Array must be of 10 values"
-        }else if( condition = 1){
+        //if to make sure numbers meet expectations
+             }
+        else if( condition = 1){
             return "Array must have numbers between 0 and 10"
-        }
+            }
+        // function running
         else{
-    let phoneNumber = arr
-    // add opening parenthesis at 0
-    phoneNumber.splice(0,0,"(")
+            let phoneNumber = arr
+            // add opening parenthesis at 0
+            phoneNumber.splice(0,0,"(")
 
-    //add closing parenthesis and space at 4
-    phoneNumber.splice(4,0,") ")
+            //add closing parenthesis and space at 4
+            phoneNumber.splice(4,0,") ")
 
-    //add - at 8
-    phoneNumber.splice(8,0,"-")
+            //add - at 8
+            phoneNumber.splice(8,0,"-")
 
-    //convert list to string
-    //return string value from list
-    return phoneNumber.join("")}
+            //convert list to string
+            //return string value from list
+            return phoneNumber.join("")
+            }
 
 }
 
